7014cff0e552873e7edb193cc1e759672d245f08
Naveen Chandran <naveenkchandran@gmail.com>
2016-04-16 14:21:58 -0400

    Row parsing

--- Housing Data/Parser.py
+++ Housing Data/Parser.py
@@ -1,10 +1,29 @@
+import csv
+
 class HousingDataParser(object):
-    
+
+
+    def __init__(self):
+        self.csvFileName = "ZipCodeMedianHousingPrices.csv"
 
 
 
-    def get
+    def getZipCodePriceTuples(self):
+        with open(self.csvFileName, "rb") as csvfile:
+            reader = csv.reader(csvfile)
 
+            print reader.next()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    parser = HousingDataParser()
+
+    parser.getZipCodePriceTuples()
 
 
 
